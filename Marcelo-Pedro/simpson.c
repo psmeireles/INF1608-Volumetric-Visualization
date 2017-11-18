5d71514a0b0718c05f1c20f00a143557c25ab215
Pedro Sousa Meireles <psmeireles25@gmail.com>
2017-11-18 16:58:14 -0200

    Changed simpson methods to fit necessities

--- Marcelo-Pedro/simpson.c
+++ Marcelo-Pedro/simpson.c
@@ -1,20 +1,20 @@
 #include "simpson.h"
 
-double DoubleSimpson (double a, double b, double (*f) (double x), double *v){
+double DoubleSimpson (double a, double b, int i, int k, unsigned char *data,  double (*f) (int i, int j, int k, unsigned char *data), double *v){
 	double sac, sab, scb, h = b-a, c = (a+b)/2, err;
-	sab = h/6*(f(a) + 4*f((a+b)/2) + f(b));
-	sac = (h/2)/6*(f(a) + 4*f((a+c)/2) + f(c));
-	scb = (h/2)/6*(f(c) + 4*f((c+b)/2) + f(b));
+	sab = h/6*(f(i, (int) a, k, data) + 4*f(i, (int) (a+b)/2, k, data) + f(i, (int) b, k, data));
+	sac = (h/2)/6*(f(i, (int) a, k, data) + 4*f(i, (int) (a+c)/2, k, data) + f(i, (int) c, k, data));
+	scb = (h/2)/6*(f(i, (int) c, k, data) + 4*f(i, (int) (c+b)/2, k, data) + f(i, (int) b, k, data));
 	err = fabs((sab - (sac + scb))/15);
 	*v = sac + scb + err;
 	return err;
 }
 
-double AdaptiveSimpson (double a, double b, double (*f) (double x), double tol){
+double AdaptiveSimpson (double a, double b, int i, int k, unsigned char *data, double (*f) (int i, int j, int k, unsigned char *data), double tol){
 	double c = (a + b)/2, err, v;
-	err = DoubleSimpson(a, b, f, &v);
+	err = DoubleSimpson(a, b, i, k, data, f, &v);
 	if(err < tol)
 		return v;
 	else
-		return AdaptiveSimpson(a, c, f, tol/2) + AdaptiveSimpson(c, b, f, tol/2);
+		return AdaptiveSimpson(a, c, i, k, data, f, tol/2) + AdaptiveSimpson(c, b, i, k, data, f, tol/2);
 }
--- Marcelo-Pedro/simpson.h
+++ Marcelo-Pedro/simpson.h
@@ -1,5 +1,5 @@
 #include <stdlib.h>
 #include <math.h>
 
-double DoubleSimpson (double a, double b, double (*f) (double x), double *v);
-double AdaptiveSimpson (double a, double b, double (*f) (double x), double tol);
+double DoubleSimpson (double a, double b, int i, int k, unsigned char *data,  double (*f) (int i, int j, int k, unsigned char *data), double *v);
+double AdaptiveSimpson (double a, double b, int i, int k, unsigned char *data, double (*f) (int i, int j, int k, unsigned char *data), double tol);
